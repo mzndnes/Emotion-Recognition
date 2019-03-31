0d09e70d801fc487c4317d2cee5f6624a09e6ea4
Shilpi <shilpi.kansal@ufl.edu>
2019-03-31 19:52:21 -0400

    added sorted to glob functions to sort the list of files/folders

--- FacialEmotionRecognition.py
+++ FacialEmotionRecognition.py
@@ -11,16 +11,16 @@ class FacialEmotionRecognition:
         self.sorted_emotion = "sorted_emotion_set"
 
     def pre_process_data(self):
-        volunteers = glob.glob("%s/*" %self.source_emotion)
+        volunteers = sorted(glob.glob("%s/*" %self.source_emotion))
         for volunteer in volunteers:
             volunteer_id = volunteer[-4:]
-            for session in glob.glob("%s/*" %volunteer):
+            for session in sorted(glob.glob("%s/*" %volunteer)):
                 session_id = session[13:17]
-                for file_name in glob.glob("%s/*"  %session):
+                for file_name in sorted(glob.glob("%s/*"  %session)):
                     file = open(file_name, 'r')
                     emotion_number = int(float(file.readline()))
-                    source_emotion_file = glob.glob("%s/%s/%s/*" %(self.source_image, volunteer_id, session_id))[-1]
-                    source_neutral_file = glob.glob("%s/%s/%s/*" %(self.source_image, volunteer_id, session_id))[0]
+                    source_emotion_file = sorted(glob.glob("%s/%s/%s/*" %(self.source_image, volunteer_id, session_id)))[-1]
+                    source_neutral_file = sorted(glob.glob("%s/%s/%s/*" %(self.source_image, volunteer_id, session_id)))[0]
                     dest_neutral_file = "%s/%s/%s" %(self.sorted_emotion, self.neutral, source_neutral_file[16:])
                     dest_emotion_file = "%s/%s/%s" %(self.sorted_emotion, self.emotions[emotion_number], source_emotion_file[16:])
                     copyfile(source_neutral_file, dest_neutral_file)
